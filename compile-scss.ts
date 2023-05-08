6bc5778d464675b45fa231870937920fe22bafde
Jeremy Banka <hello@jeremybanka.com>
2023-05-07 23:43:46 -0700

    🔨 compile icons and fonts

--- compile-scss.ts
+++ compile-scss.ts
@@ -1,33 +1,56 @@
-import * as fs from 'fs-extra';
-import * as path from 'path';
-import sass from 'sass';
+import * as path from "path"
 
-const sourceDirectory = './src/fonts';
-const targetDirectory = './packages';
-const scssExtension = '.scss';
+import * as fs from "fs-extra"
+import sass from "sass"
+
+const ASSET_GROUPS = [
+  { packagePrefix: `fonts-`, sourceDirectory: `fonts`, sourceSuffix: `-font` },
+  { packagePrefix: `icons-`, sourceDirectory: `icons`, sourceSuffix: `-icon` },
+] as const
+
+const SOURCE_DIR = `./src`
+const TARGET_DIR = `./packages`
+const SCSS_EXTENSION = `.scss`
 
 async function compileScssFiles(): Promise<void> {
   try {
-    const files = await fs.readdir(sourceDirectory);
-    const scssFiles = files.filter(file => path.extname(file) === scssExtension);
-    console.log(`Found ${scssFiles.length} SCSS files in ${sourceDirectory}`);
+    for (const {
+      packagePrefix,
+      sourceDirectory,
+      sourceSuffix,
+    } of ASSET_GROUPS) {
+      const assetGroupSourceDir = path.join(SOURCE_DIR, sourceDirectory)
+      const files = await fs.readdir(assetGroupSourceDir)
+      const scssFiles = files.filter(
+        (file) => path.extname(file) === SCSS_EXTENSION
+      )
+      console.log(
+        `Found ${scssFiles.length} SCSS files in ${assetGroupSourceDir}`
+      )
 
-    for (const file of scssFiles) {
-      const scssFilePath = path.join(sourceDirectory, file);
-      const fileNameWithoutExt = path.basename(file, scssExtension);
-      const fileNameWithoutSuffix = fileNameWithoutExt.replace('-font', '');
-      const cssTargetDirectory = path.join(targetDirectory, `fonts-` + fileNameWithoutSuffix);
+      for (const file of scssFiles) {
+        const scssFilePath = path.join(assetGroupSourceDir, file)
+        const fileNameWithoutExt = path.basename(file, SCSS_EXTENSION)
+        const fileNameWithoutSuffix = fileNameWithoutExt.replace(
+          sourceSuffix,
+          ``
+        )
+        const cssTargetDirectory = path.join(
+          TARGET_DIR,
+          packagePrefix + fileNameWithoutSuffix
+        )
 
-      await fs.ensureDir(cssTargetDirectory);
+        await fs.ensureDir(cssTargetDirectory)
 
-      const result = sass.compile(scssFilePath)
-      const cssOutputPath = path.join(cssTargetDirectory, `font-face.css`);
-      fs.writeFile(cssOutputPath, result.css);
-      console.log(`Compiled ${file} to ${cssOutputPath}`);
+        const result = sass.compile(scssFilePath)
+        const cssOutputPath = path.join(cssTargetDirectory, `font-face.css`)
+        fs.writeFile(cssOutputPath, result.css)
+        console.log(`Compiled ${file} to ${cssOutputPath}`)
+      }
     }
   } catch (error) {
-    console.error('Error while processing SCSS files:', error);
+    console.error(`Error while processing SCSS files:`, error)
   }
 }
 
-compileScssFiles();
+compileScssFiles()
