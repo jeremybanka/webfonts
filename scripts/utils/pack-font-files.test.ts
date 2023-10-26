80e06a192381e9dd2074d9f9170dc569f22f7094
Jeremy Banka <hello@jeremybanka.com>
2023-10-25 18:07:57 -0700

    ♻️ refactor lockfile function and test

--- scripts/utils/pack-font-files.test.ts
+++ scripts/utils/pack-font-files.test.ts
@@ -1,29 +1,38 @@
-import { mkdirSync, writeFileSync } from "fs"
+import { existsSync, mkdirSync, writeFileSync } from "fs"
 
-import * as fs from "fs-extra"
 import tmp from "tmp"
 
 import { packFonts } from "./pack-font-files"
+import { quickFileTree } from "./quick-file-tree"
 
 let tmpDir: tmp.DirResult
 
 beforeEach(() => {
   tmpDir = tmp.dirSync({ unsafeCleanup: true })
-  mkdirSync(`${tmpDir.name}/packages`)
-  mkdirSync(`${tmpDir.name}/packages/fonts-ergata`)
-  mkdirSync(`${tmpDir.name}/export`)
-  writeFileSync(`${tmpDir.name}/export/ergata-4.otf`, `1234567890`)
+  quickFileTree(
+    {
+      packages: {
+        "fonts-ergata": {
+          "ergata-4.otf": `1234567890`,
+        },
+      },
+      export: {
+        "ergata-4.otf": `1234567890`,
+      },
+    },
+    tmpDir.name
+  )
   tmp.setGracefulCleanup()
 })
 
 describe(`pack font files`, () => {
   it(`moves files from export/ into their corresponding packages/`, async () => {
     await packFonts(tmpDir.name)
-    expect(
-      fs.existsSync(`${tmpDir.name}/packages/fonts-ergata/ergata-4.otf`)
-    ).toBe(true)
-    expect(
-      fs.existsSync(`${tmpDir.name}/export/fonts-ergata/ergata-4.otf`)
-    ).toBe(false)
+    expect(existsSync(`${tmpDir.name}/packages/fonts-ergata/ergata-4.otf`)).toBe(
+      true
+    )
+    expect(existsSync(`${tmpDir.name}/export/fonts-ergata/ergata-4.otf`)).toBe(
+      false
+    )
   })
 })
